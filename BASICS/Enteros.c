6453505b6a88c93b568927d564458d1569b07fb4
Íker Lloréns <iker.llorens.13@gmail.com>
2022-07-02 10:12:43 -0500

    Added examplie for Tipos.c Added example for int overflow

---
+++ BASICS/Enteros.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int Entero = 0;
+    while (Entero > 0)
+    {
+        Entero += 1;
+    }
+
+    printf("El valor de enter es %d", Entero);
+
+    return 0;
+}
--- BASICS/Tipos.c
+++ BASICS/Tipos.c
@@ -3,12 +3,19 @@
 
 int main(void)
 {
-	int Entero = 0;
+	
+	int Entero = 13;
 	char Carater = 'i';
+	// También conocido como string o cadnea de caracteres
+	char[15] UnArregloDeCaracteres = {'H', 'o', 'l', 'a', ' ', 'M', 'u', 'n', 'd', 'o'}; 
 	// void ;
 	float PuntoFlotante = 13.4;
-	
-	
+	int* PunteroAEntero = &Entero; // Para más información de punteros, ver los ejercicios de punteros
+	int[5] UnArreglo = {1, 2, 3, 4, 5}; // Un arreglo de memoria, para saber más de arreglos ver los ejercicios de arreglos
+
+	// STUDENTS: Cuanta memoria RAM ocuparemos con este programa?
+
+
 
 	return 0;	
 }
