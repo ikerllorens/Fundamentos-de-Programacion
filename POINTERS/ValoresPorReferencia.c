edc2de86ecc3ddb99a4a43bd1de2f1c6811c7d8c
Íker Lloréns <iker.llorens.13@gmail.com>
2022-08-06 10:38:22 -0500

    created example. missing docs

--- POINTERS/ValoresPorReferencia.c
+++ POINTERS/ValoresPorReferencia.c
@@ -4,5 +4,20 @@ void Suma(int a, int b, int *resultado);
 
 int main(void)
 {
-	
-	j
+	int VariableResultado;
+	int numero1;
+	int numero2;
+
+	Suma(numero1, numero2, &VariableResultado);
+
+	return 0;
+}
+
+void Suma(int a, int b, int *resultado)
+{
+	*resultado = a + b;
+	a = a + 1;
+	b = b + 1;
+
+	return;
+}
