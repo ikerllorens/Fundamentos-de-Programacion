7728d72ad380a4d59003a3b0f3a71800921df8ce
Iker Llorens <illorenslara@microsoft.com>
2023-08-30 17:10:33 -0600

    Putting order on the repo

--- EJEMPLOS_ARQUITECTURA/arregloBidimensional.c
+++ EJEMPLOS_ARQUITECTURA/arregloBidimensional.c
@@ -11,8 +11,8 @@
  * @author Desarrollador que realizó la última modificación
  *   Thalía Bravo
  * @date Fecha de última modificación
- *   24 de noviembre de 2020   
-*/
+ *   24 de noviembre de 2020
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,8 +22,9 @@
 #define COLUMNAS 50
 #define FILAS 50
 
-
-/**@brief Prototipos de función*/
+/*
+ * @brief Prototipos de función
+ */
 void mostrarInstrucciones(char nombre[], char inst[]);
 void limpiarEntradaDatos(void);
 int generarNumerosAleatorios(int, int);
@@ -31,63 +32,62 @@ int pedirNumero(int, int);
 void llenarMatriz(int *, int *, float matriz[FILAS][COLUMNAS]);
 void desplegarMatriz(int, int, float matriz[FILAS][COLUMNAS]);
 
-  
 /**
  * @fn Función del programa principal
  * @return Entero: Si muestra un valor 0 nuestro programa, se está ejecutando correctamente.
-*/
+ */
 
-
-int main(void) 
+int main(void)
 {
   float matriz[FILAS][COLUMNAS];
   int filas, columnas;
-  
+
   mostrarInstrucciones("Thalia Bravo", "Programa que muestra el uso de arreglos de dos dimensiones o bidimensionales.");
 
   llenarMatriz(&filas, &columnas, matriz);
 
   desplegarMatriz(filas, columnas, matriz);
-  
+
   return 0;
 }
 
-  /**
-   *@fn Procedimiento Muestra las instrucciones del programa.
-   */
-void mostrarInstrucciones(char autores[], char instrucciones [])
+/**
+ *@fn Procedimiento Muestra las instrucciones del programa.
+ */
+void mostrarInstrucciones(char autores[], char instrucciones[])
 {
-    printf("Aplicación desarrollada por:\n");
-    printf("\t%s\n\n", autores);
-    printf("Descripción del programa:\n");
-    printf("\t%s\n\n", instrucciones);
-    printf("Presiona la tecla \"enter\" para continuar.\n");
-    getchar();
-    
-    system("clear");
-    
-  return; 
+  printf("Aplicación desarrollada por:\n");
+  printf("\t%s\n\n", autores);
+  printf("Descripción del programa:\n");
+  printf("\t%s\n\n", instrucciones);
+  printf("Presiona la tecla \"enter\" para continuar.\n");
+  getchar();
+
+  system("clear");
+
+  return;
 }
 
 /**
  *@fn Procedimiento que limpia el buffer de entrada.
  */
-  void limpiarEntradaDatos(void)
+void limpiarEntradaDatos(void)
 {
   int limpiar;
 
-  while((limpiar = getchar()) != '\n' && limpiar != EOF) {}
+  while ((limpiar = getchar()) != '\n' && limpiar != EOF)
+  {
+  }
 
   return;
 }
-
 
 /**
  *@fn Función que genera números aleatorios
  *@param Entero rango inicio, Entero rango final
  *@return Número entero aleatorio
  */
-int generarNumerosAleatorios (int inicio, int final)
+int generarNumerosAleatorios(int inicio, int final)
 
 {
   int aleatorio;
@@ -96,7 +96,6 @@ int generarNumerosAleatorios (int inicio, int final)
 
   return aleatorio;
 }
-
 
 /**
  *@fn Función que recibe un número entero entre un número mínimo y máximo.
@@ -108,32 +107,31 @@ int pedirNumero(int min, int max)
 
   do
   {
-     scanf(" %d", &num);
-     if(num<min || num>max)
-     {
-       printf("ERROR: Tu número debe estar entre %d y %d.\n", min, max);
-       printf("Intenta de nuevo: ");
-     }
-  }while(num<min || num>max);
+    scanf(" %d", &num);
+    if (num < min || num > max)
+    {
+      printf("ERROR: Tu número debe estar entre %d y %d.\n", min, max);
+      printf("Intenta de nuevo: ");
+    }
+  } while (num < min || num > max);
 
-     return num;
+  return num;
 }
-
 
 /**
  *@fn Procedimiento que llena una matriz con un mínimo y un máximo de filas y columnas.
  *@param Enteros para filas y columnas, matriz con un máximo de filas y colunas.
-*/
+ */
 
 void llenarMatriz(int *filas, int *columnas, float matriz[FILAS][COLUMNAS])
 {
-/**
- *@var i representa las filas
- *@var j representa las columnas
- */
+  /**
+   *@var i representa las filas
+   *@var j representa las columnas
+   */
 
   int i, j;
-  
+
   printf("Dime de cuántas filas va a ser la matriz (%d...%d): ", MIN_FILAS, FILAS);
   *filas = pedirNumero(MIN_FILAS, FILAS);
 
@@ -142,31 +140,30 @@ void llenarMatriz(int *filas, int *columnas, float matriz[FILAS][COLUMNAS])
 
   printf("\n");
 
-  for(i=0; i<*filas; i++)
+  for (i = 0; i < *filas; i++)
   {
-    for(j=0; j<*columnas; j++)
+    for (j = 0; j < *columnas; j++)
     {
       printf("matriz[%d][%d] = ", i, j);
       scanf(" %f", &matriz[i][j]);
     }
   }
-  
+
   return;
 }
 
 /**
  *@fn Procedimiento que muestra los valores que hay dentro d euna matriz.
  *@param filas, columnas, matriz de números reales.
-*/
-
+ */
 void desplegarMatriz(int filas, int columnas, float matriz[FILAS][COLUMNAS])
 
 {
   int i, j;
   printf("\n\nLa matriz que se ingresó fue:\n");
-  for(i=0; i<filas; i++)
+  for (i = 0; i < filas; i++)
   {
-    for(j=0; j<columnas; j++)
+    for (j = 0; j < columnas; j++)
     {
       printf(" %5.2f\t ", matriz[i][j]);
     }
