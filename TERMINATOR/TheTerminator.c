117e2dd5571f32e12d3db9ab0609604b7d6a138c
Iker Llorens <illorenslara@microsoft.com>
2023-09-04 17:12:06 -0600

    Commiting changes

--- TERMINATOR/TheTerminator.c
+++ TERMINATOR/TheTerminator.c
@@ -2,29 +2,91 @@
 #include <string.h>
 #include <stdlib.h>
 
-int IsFileInDirectory(char directory[], char student[]);
+int is_file_in_directory(char file[], char student[]);
+void log(char msg[], char local_dir_name[]);
+int BuildStudentsProgram(char directory[], char student[]);
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    FILE *fp = fopen("./Lista.txt", "r");
-    char account[16] = "";
+    FILE *fp;
 
-    char cmd[256] = "/TAREAS/TAREA1/tarea1.c";
-    system("mkdir TAREA1");
+    char account[16] = "";
+    char cmd[256] = "";
+
+    if (argc != 4)
+    {
+        printf("Uso: Terminator.out <lista alumnos> <ruta del archivo a calificar> <nombre local> \n");
+        return 0;
+    }
+
+    strcat("mkdir ", argv[3]);
+
+    fp = fopen(argv[1], "r");
 
     while (fscanf(fp, "%s", account) != EOF)
     {
-        printf("> %s\n", account);
-        if (IsFileInDirectory(cmd, account) != 0)
+        log("-> Evaluando %s", argv[3]);
+        if (is_file_in_directory(argv[2], account) == 0)
         {
-            printf("Hubo problema con %s \n", account);
+        }
+        else
+        {
+            log("No se enocntro el archivo para ", argv[3]);
+            log(account, argv[3]);
         }
     }
+
+    fclose(fp);
 
     return 0;
 }
 
-int IsFileInDirectory(char directory[], char student[])
+/*
+ * Escribe un mensaje en el archivo de log y en pantalla
+ * @param msg Mensaje a escribir
+ * @param local_dir_name Nombre del directorio local
+ */
+void log(char msg[], char local_dir_name[])
+{
+    FILE *log;
+    char path[256] = "./";
+    strcat(path, local_dir_name);
+    strcat(path, "/log.txt");
+
+    log = fopen(path, "a");
+
+    printf("> %s\n", msg);
+    fprintf(log, "> %s\n", msg);
+
+    fclose(log);
+}
+
+/*
+ * Verifica si un archivo existe en un directorio
+ * @param file Directorio y nombre del archivo a buscar
+ * @param student Cuenta del alumno a buscar
+ *
+ * @return 1 si el archivo existe, 0 si no existe
+ */
+int is_file_in_directory(char file[], char student[])
+{
+    char path[256] = "/home/";
+    FILE *fp;
+
+    strcat(path, student);
+    strcat(path, "/");
+    strcat(path, file);
+
+    fp = fopen(path, "r");
+    if (fp == NULL)
+    {
+        return 0;
+    }
+
+    return 1;
+}
+
+int BuildStudentsProgram(char directory[], char student[])
 {
     char ext[4] = ".c";
     char out[256] = "";
@@ -44,7 +106,9 @@ int IsFileInDirectory(char directory[], char student[])
     pclose(fp);
 
     if (strlen(out) != 0)
+    {
         printf("ERROR con %s : %s \n", student, out);
+    }
 
     return strlen(out);
 }
